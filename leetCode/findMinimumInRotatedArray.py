e6666538cccdfff902b2b89212320f084a9e49de
agrim08 <agrimgupta0805@gmail.com>
2024-11-29 20:47:36 +0530

    Leetcode 153

---
+++ leetCode/findMinimumInRotatedArray.py
@@ -0,0 +1,12 @@
+def findMinInRotatedArray(nums):
+    n = len(nums)
+    l = 0
+    r = n - 1
+    while l < r:
+        m = (l + r) // 2
+        if nums[m] < nums[r]:
+            l = m + 1
+        else:
+            r = m
+    
+    return nums[r]
