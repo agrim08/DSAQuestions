001f8c2b5c9e04c8bfcd9cb0f9beeca04a06c79b
agrim08 <agrimgupta0805@gmail.com>
2024-12-04 23:02:35 +0530

    leetcode 83

---
+++ leetCode/removeDuplicatesFromSortedList.py
@@ -0,0 +1,11 @@
+#leetcode 83
+
+def deleteDuplicates(self, head):
+        cur = head
+
+        while cur and cur.next:
+            if cur.val  == cur.next.val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+        return head
