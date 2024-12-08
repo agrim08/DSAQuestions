81faa33c2efc0f52fd216b6df2052656276c2e3c
agrim08 <agrimgupta0805@gmail.com>
2024-12-09 01:18:59 +0530

    leetcode 206

---
+++ leetCode/reverseALinkedList.py
@@ -0,0 +1,13 @@
+# leetcode 206
+
+def reverseList(head):
+    cur = head
+    prev = None
+
+    while cur:
+        temp = cur.next
+        cur.next = prev
+        prev = cur
+        cur = temp
+        
+    return prev
