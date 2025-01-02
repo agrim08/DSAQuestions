9652dbe1f4a6b19c8b5e181ffdb19afb1201e462
agrim08 <agrimgupta0805@gmail.com>
2025-01-02 23:39:48 +0530

    leetcode 1

---
+++ leetCode/2sum.py
@@ -0,0 +1,12 @@
+#leetcode 1
+def twoSum(self, nums, target):
+        h = {}
+         
+
+        for i in range(len(nums)):
+            h[nums[i]] = i
+        for i in range(len(nums)):
+            y = target - nums[i]
+            
+            if y in h and h[y] != i:
+                return [i , h[y]]
