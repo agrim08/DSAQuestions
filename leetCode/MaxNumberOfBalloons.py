056d44d4f8e77dd995aa5887a44ad916c032b646
agrim08 <agrimgupta0805@gmail.com>
2025-01-19 22:58:00 +0530

    leetcode 1189

---
+++ leetCode/MaxNumberOfBalloons.py
@@ -0,0 +1,17 @@
+#leetcode 1189
+
+from collections import defaultdict
+
+def maxNumberOfBalloons(self, text: str) -> int:
+        counter = defaultdict(int)
+        balloon = 'balloon'
+
+        for c in text:
+            if c in balloon:
+                counter[c] += 1
+        
+        if(any(c not in counter for c in balloon)):
+            return 0
+        
+        else:
+            return min(counter['b'], counter['a'], counter['l']//2, counter['o']//2, counter['n'])
