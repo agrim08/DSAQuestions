ba6a905ce940f6da94f48a66ffca65a70722c149
agrim08 <agrimgupta0805@gmail.com>
2025-02-01 22:41:56 +0530

    leetcode 28

---
+++ leetCode/leetcode28.py
@@ -0,0 +1,24 @@
+# aaproach 1
+def strStr(self, haystack: str, needle: str) -> int:
+        if needle == "":
+            return 0
+        
+        for i in range(len(haystack) + 1 - len(needle)):
+            for j in range(len(needle)):
+                if haystack[i + j] != needle[j]:
+                    break
+                if j == len(needle) - 1:
+                    return i
+        
+        return -1
+
+#approach 2
+def strStr(self, haystack: str, needle: str) -> int:
+        if needle == "":
+            return 0
+        
+        for i in range(len(haystack) + 1 - len(needle)):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        
+        return -1    
