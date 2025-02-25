b1d62d091800971f40baf990bedbb4f88edd1897
agrim08 <agrimgupta0805@gmail.com>
2025-02-25 22:49:08 +0530

    leetcode 739

---
+++ leetCode/leetcode_739.py
@@ -0,0 +1,16 @@
+#daily temperatures
+
+def dailyTempratures(temperatures: List[int]) -> List[int]:
+    n = len(temperatures)
+    ans = [0] * n
+    stk = []
+    
+    for i, t in enumerate(temperatures):
+        while stk and stk[-1][0] > t:
+            stk_t, stk_i = stk.pop()
+            ans[stk_i] = i - stk_i
+        
+        stk.append((t, i))
+    
+    return ans
+        
