1e63ca56f2be47ca6548147b9ebe7cb2e1381abc
agrim08 <agrimgupta0805@gmail.com>
2025-08-08 11:54:54 +0530

    rotate list

---
+++ namaste_dsa/LinkedLists/RotateList.js
@@ -0,0 +1,31 @@
+var rotateRight = function(head, k) {
+    if(!head || !head.next) return head
+
+    let length = 0
+    let curr = head
+
+    while(curr){
+        curr = curr.next
+        length++
+    }
+
+    k = k % length
+
+    let slow = head
+    let fast = head
+
+    for(let i = 0; i < k; i++){
+        fast = fast.next
+    }
+
+    while(fast.next){
+        slow = slow.next
+        fast = fast.next
+    }
+
+    fast.next = head
+    let newHead = slow.next
+    slow.next = null
+
+    return newHead
+};
