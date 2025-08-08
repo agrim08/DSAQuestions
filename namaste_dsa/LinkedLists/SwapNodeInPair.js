ab195f7b758ab3c9158bd87fa60413483fb15a17
agrim08 <agrimgupta0805@gmail.com>
2025-08-08 21:58:31 +0530

    Linked List

---
+++ namaste_dsa/LinkedLists/SwapNodeInPair.js
@@ -0,0 +1,21 @@
+var swapPairs = function(head) {
+    if(!head || !head.next) return head
+
+    let dummy = new ListNode()
+    dummy.next = head
+    let prev = dummy
+    let curr = head
+    let N = head.next
+
+    while(curr && N){
+        prev.next = N
+        curr.next = N.next
+        N.next = curr
+
+        prev = curr
+        curr = prev.next
+        N = curr && curr.next
+    }
+
+    return dummy.next
+};
