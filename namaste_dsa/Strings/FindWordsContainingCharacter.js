99580ea9f2e5bf8b893ca6c910f5d850915ed0a6
agrim08 <agrimgupta0805@gmail.com>
2025-08-10 12:38:06 +0530

    leetcode 2942

---
+++ namaste_dsa/Strings/FindWordsContainingCharacter.js
@@ -0,0 +1,10 @@
+var findWordsContaining = function(words, x) {
+    let ans = []
+    for(let i = 0; i < words.length; i++){
+        if(words[i].includes(x)){
+            ans.push(i)
+        }
+    }
+
+    return ans
+};
--- namaste_dsa/Strings/LengthOfLastWord.js
+++ namaste_dsa/Strings/LengthOfLastWord.js
@@ -14,3 +14,21 @@ var lengthOfLastWord = function(s) {
 
     return count
 };
+
+//in one loop
+
+function lengthOfLastWord(s) {
+    let count = 0
+    let n = s.length - 1
+
+    while(n >= 0){
+        if(s[n] != " "){
+            count++
+        }else if(count > 0){
+            break
+        }
+        n--
+    }
+
+    return count
+}
