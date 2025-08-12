4108dc03930d128bea667d9244c801b0348c09b0
agrim08 <agrimgupta0805@gmail.com>
2025-08-12 13:25:54 +0530

    leetcode 49

---
+++ namaste_dsa/Strings/GroupAnagrams.js
@@ -0,0 +1,16 @@
+//approach 1
+
+var groupAnagrams = function(strs) {
+    let map = {}
+
+    for(let i = 0; i < strs.length; i++){
+        let sorted = strs[i].split("").sort().join("")
+        if(!map[sorted]){
+            map[sorted] = [strs[i]]
+        }else{
+            map[sorted].push(strs[i])
+        }
+    }
+
+    return [...Object.values(map)]
+};
