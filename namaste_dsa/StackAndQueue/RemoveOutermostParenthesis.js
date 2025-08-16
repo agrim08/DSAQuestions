9b42fcb6e514126eb09460391247ec4d8be9cd59
agrim08 <agrimgupta0805@gmail.com>
2025-08-16 23:04:29 +0530

    leetcode 1021 optimized

--- namaste_dsa/StackAndQueue/RemoveOutermostParenthesis.js
+++ namaste_dsa/StackAndQueue/RemoveOutermostParenthesis.js
@@ -14,3 +14,20 @@ var removeOuterParentheses = function(s) {
 
     return ans
 };
+
+//Approach 2 without stack
+var removeOuterParenthesesOptimized = function(s) {
+    let level = 0
+    let ans = ""
+
+    for(let i = 0; i < s.length; i++){
+        if(s[i] === "("){
+            level++
+            ans += (level > 1) ? s[i] : ""
+        }else{
+            ans += (level > 1) ? s[i] : ""
+            --level
+        }
+    }
+    return ans
+};
