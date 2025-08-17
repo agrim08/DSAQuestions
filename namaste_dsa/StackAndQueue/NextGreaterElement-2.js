fd9e704feb3b416325b1049e1b9908d4a5ff23e7
agrim08 <agrimgupta0805@gmail.com>
2025-08-17 19:32:53 +0530

    leetcode 503

---
+++ namaste_dsa/StackAndQueue/NextGreaterElement-2.js
@@ -0,0 +1,48 @@
+var nextGreaterElements = function(nums) {
+    let arr = [...nums, ...nums]
+    let n = arr.length
+    let s = []
+    let ans = new Array(n).fill(-1)
+    s.push(arr[n - 1])
+
+    for(let i = n - 2; i >= 0; i--){
+        while(s.length){
+            let top = s[s.length - 1]
+            if(arr[i] < top){
+                ans[i] = top
+                break
+            }else{
+                s.pop()
+            }
+        }
+        s.push(arr[i])
+    }
+
+    return ans.slice(0, nums.length)
+};
+
+
+
+//optimized:-
+//! try to dry run
+var nextGreaterElementsOptimized = function(arr) {
+    let n = arr.length
+    let s = []
+    let ans = new Array(n).fill(-1)
+    s.push(arr[n - 1])
+
+    for(let i = (2 * n) - 2; i >= 0; i--){
+        while(s.length){
+            let top = s[s.length - 1]
+            if(arr[i % n] < top){
+                ans[i % n] = top
+                break
+            }else{
+                s.pop()
+            }
+        }
+        s.push(arr[i % n])
+    }
+
+    return ans
+};
