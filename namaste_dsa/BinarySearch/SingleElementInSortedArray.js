5d8ee8b9e5b03aa2aa1eb7d528e07a8de7c262e5
agrim08 <agrimgupta0805@gmail.com>
2025-08-23 23:09:50 +0530

    leetcode 540

---
+++ namaste_dsa/BinarySearch/SingleElementInSortedArray.js
@@ -0,0 +1,30 @@
+const singleNonDuplicate = function(arr){
+    let l = 0
+    let r = arr.length - 1
+
+    while (l <= r) {
+        let m = l +  Math.floor((r - l) / 2)
+        if (arr[m] === arr[m - 1]){
+            let leftCount = m - l - 1
+            if(leftCount % 2 === 1){
+                r = m - 2
+            }else{
+                l = m + 1
+            }
+        }
+        else if(arr[m] === arr[m + 1]){
+            let leftCount = m - l
+            if(leftCount % 2 === 1){
+                r = m - 1
+            }else{
+                l = m + 2
+            }
+        }
+        else{
+            return arr[m]
+        }
+    }
+}
+
+console.log(singleNonDuplicate([1,1,2,3,3,4,4,8,8]));
+console.log(singleNonDuplicate([3,3,7,7,10,11,11]));
