31f8e8633d8e91092cd2552f0820ecf9217e3a30
agrim08 <agrimgupta0805@gmail.com>
2025-08-24 02:48:13 +0530

    leetcode 658

---
+++ namaste_dsa/BinarySearch/FindKClosestElements.js
@@ -0,0 +1,20 @@
+var findClosestElements = function(arr, k, x) {
+    let l = 0
+    let r = arr.length - 1
+
+    while(l < r){
+        let m = l + Math.floor((r - l) / 2)
+        if((arr[m + k] - x) < (x - arr[m])){
+            l = m + 1
+        }else{
+            r = m
+        }
+    }
+
+    let ans = []
+    for(let i = 0; i < l + k; i++){
+        ans.push(arr[i])
+    }
+    return ans
+//  return arr.slice(l , l + k)
+};
