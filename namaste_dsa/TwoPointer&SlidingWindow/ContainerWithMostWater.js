ad2d6f0444a088496a7af3dd934954c1e42cad03
agrim08 <agrimgupta0805@gmail.com>
2025-08-28 22:42:56 +0530

    leetcode 11

---
+++ namaste_dsa/TwoPointer&SlidingWindow/ContainerWithMostWater.js
@@ -0,0 +1,18 @@
+var maxArea = function(height) {
+    let i = 0
+    let j = height.length - 1
+    let maxWater = 0
+
+    while(i < j){
+        let area = Math.min(height[i], height[j]) * (j - i)
+        maxWater = Math.max(maxWater, area)
+
+        if(height[i] > height[j]){
+            j--
+        }else{
+            i++
+        }
+    }
+
+    return maxWater
+};
