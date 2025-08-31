84225a9a77533c95626b7f7bedcffe93ecf3ecfd
agrim08 <agrimgupta0805@gmail.com>
2025-08-31 19:41:20 +0530

    leetcode 42

---
+++ namaste_dsa/TwoPointer&SlidingWindow/TrappingRainWater.js
@@ -0,0 +1,28 @@
+var trap = function(arr) {
+    let n = arr.length
+
+    let maxL = []
+    maxL[0] = arr[0]
+
+    for(let i = 1; i < n; i++){
+        maxL[i] = Math.max(maxL[i - 1], arr[i])
+    }
+
+    let maxR = []
+    maxR[n - 1] = arr[n - 1]
+    for(let i = n - 2; i >= 0; i--){
+        maxR[i] = Math.max(arr[i], maxR[i + 1])
+    }
+
+    console.log(maxR)
+    console.log(maxL)
+
+    let ans = 0
+    for(let i = 0; i < n; i++){
+        let waterTrapped = (Math.min(maxL[i], maxR[i])) - arr[i]
+        ans = ans + Math.max(waterTrapped, 0)
+    }
+
+    return ans
+
+};
