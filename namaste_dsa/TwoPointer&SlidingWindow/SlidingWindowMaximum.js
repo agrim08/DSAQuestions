8e5f06b821f0b2af651f994790053aea0a82e416
agrim08 <agrimgupta0805@gmail.com>
2025-09-05 22:52:41 +0530

    leetcode 239

---
+++ namaste_dsa/TwoPointer&SlidingWindow/SlidingWindowMaximum.js
@@ -0,0 +1,22 @@
+var maxSlidingWindow = function(nums, k) {
+    let res = []
+    let q = []
+
+    let i = j = 0
+    while(j < nums.length){
+        while(q.length && nums[j] > q[q.length - 1]){
+            q.pop()
+        }
+        q.push(nums[j])
+
+        if(j >= k - 1){
+            res.push(q[0])
+
+            nums[i] == q[0] && q.shift()
+            ++i
+        }
+        ++j
+    }
+
+    return res
+};
