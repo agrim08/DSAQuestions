0737f9068b3c9b9d4480af0e0ea3fb76a437c669
agrim08 <agrimgupta0805@gmail.com>
2025-09-06 23:07:43 +0530

    leetcode 424

--- namaste_dsa/TwoPointer&SlidingWindow/LongestRepeatingCharacterReplacement.js
+++ namaste_dsa/TwoPointer&SlidingWindow/LongestRepeatingCharacterReplacement.js
@@ -1,26 +1,33 @@
 var characterReplacement = function(s, k) {
-    let i = 0, j = 0;
-    let map = Array(26).fill(0);
-    map[s.charCodeAt(0) - 65] = 1;
-    let maxWindow = 0;
-    while (j < s.length) {
-        if (isWindowValid(map, k)) {
-            maxWindow = Math.max(maxWindow, j - i + 1);
-            ++j;
-            ++map[s.charCodeAt(j) - 65];
-        } else {
-            --map[s.charCodeAt(i) - 65];
-            ++i;
+    let i = 0, j = 0
+    let map = {}
+    map[s[i]] = 1
+    let maxWindow = 0
+
+    while(j < s.length){
+        if(isWindowValid(map, k)){
+            maxWindow = Math.max(maxWindow, (j - i + 1))
+            ++j
+            map[s[j]] = !map[s[j]] ? 1 : ++map[s[j]]
+        }else{
+            --map[s[i]]
+            ++i
         }
     }
-    return maxWindow;  
+
+    return maxWindow
 };
-var isWindowValid = function(map, k) {
-    let totalCount = 0;
-    let maxCount = 0;
-    for (let i = 0; i < 26; i++) {
-        totalCount += map[i];
-        maxCount = Math.max(maxCount, map[i]);
+
+var isWindowValid = function(map, k){
+    let totalCount  = 0
+    let maxCount = 0
+    for(let i = 0; i < 26; i++){
+        let char = String.fromCharCode(65 + i)
+        if(map[char]){
+            totalCount = totalCount + map[char]
+            maxCount = Math.max(maxCount , map[char])
+        }
     }
-    return (totalCount - maxCount <= k);
-};
+
+    return (totalCount - maxCount <= k)
+}
