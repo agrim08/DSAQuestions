51a046c4ffefdf0045fed122abc578bc43239059
agrim08 <agrimgupta0805@gmail.com>
2025-09-17 23:23:22 +0530

    postorder iterative

--- namaste_dsa/BinaryTree/IterativeTraversal.js
+++ namaste_dsa/BinaryTree/IterativeTraversal.js
@@ -32,3 +32,25 @@ var inorderTraversal = function(root) {
 
     return ans
 };
+
+
+var postorderTraversal = function(root) {
+    if(!root) return []
+    let s1 = [root]
+    let s2 = []
+
+    while(s1.length){
+        let curr = s1.pop()
+        s2.push(curr)
+
+        curr.left && s1.push(curr.left)
+        curr.right && s1.push(curr.right)
+    }
+
+    let ans = []
+    while(s2.length){
+        ans.push(s2.pop().val)
+    }
+
+    return ans
+};
