7c40f610722b3b0c739563221d07be8c7e0233cb
agrim08 <agrimgupta0805@gmail.com>
2025-11-25 02:18:13 +0530

    leetcode 90

---
+++ namaste_dsa/Backtracking/subset2.js
@@ -0,0 +1,40 @@
+//this will fail if array is not sorted, [2,1] → "2,1", [1,2] → "1,2"
+
+var subsetsWithDup = function(arr) {
+    let res = []
+    let used = new Set()
+    let backtrack = (path, start) => {
+        let key = path.join(',')
+
+        if(!used.has(key)){
+            used.add(key)
+            res.push([...path])
+        }
+
+        for(let i = start; i < arr.length; i++){
+            path.push(arr[i])
+            backtrack(path, i + 1)
+            path.pop()
+        }
+    }
+    backtrack([], 0)
+    return res
+};
+
+//optimized approach without using extra space for set
+var subsetsWithDup = function(arr) {
+    let res = []
+    arr.sort((a,b) => a - b)
+    let backtrack = (path, start) => {
+        res.push([...path])
+
+        for(let i = start; i < arr.length; i++){
+            if(i > start && arr[i] === arr[i - 1]) continue;
+            path.push(arr[i])
+            backtrack(path, i + 1)
+            path.pop()
+        }
+    }
+    backtrack([], 0)
+    return res
+};
