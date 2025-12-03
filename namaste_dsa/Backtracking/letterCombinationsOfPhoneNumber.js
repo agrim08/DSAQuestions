45466f7fc52f2015292fff61bfa3399fdce82143
agrim08 <agrimgupta0805@gmail.com>
2025-12-03 21:34:37 +0530

    leetcode 17

---
+++ namaste_dsa/Backtracking/letterCombinationsOfPhoneNumber.js
@@ -0,0 +1,33 @@
+var letterCombinations = function(digits) {
+    if(!digits.length) return []
+    const letters = {
+        2 : "abc",
+        3 : "def",
+        4 : "ghi",
+        5 : "jkl",
+        6 : "mno",
+        7 : "pqrs",
+        8 : "tuv",
+        9 : "wxyz"
+    }
+
+    let res = []
+
+    let backtrack = (path, index) => {
+        if(index === digits.length){
+            res.push(path.join(""))
+            return
+        }
+
+        let choices = letters[digits[index]]
+
+        for(let i = 0; i < choices.length; i++){
+            path.push(choices[i])
+            backtrack(path, index + 1)
+            path.pop()
+        }
+    }
+
+    backtrack([], 0)
+    return res
+};
