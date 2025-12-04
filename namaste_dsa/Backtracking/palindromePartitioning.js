689f1518ddf89ea9b199e58869d6b905ca58a0c1
agrim08 <agrimgupta0805@gmail.com>
2025-12-05 01:26:52 +0530

    leetcode 131

---
+++ namaste_dsa/Backtracking/palindromePartitioning.js
@@ -0,0 +1,35 @@
+var partition = function (s) {
+    let res = []
+
+    let backtrack = (path, remainingString) => {
+        if(!remainingString.length){
+            res.push([...path])
+            return
+        }
+
+        for (let i = 1; i <= remainingString.length; i++) {
+            let choice = remainingString.substring(0, i)
+
+            if (!isPalindrome(choice)) continue;
+            path.push(choice)
+            backtrack(path, remainingString.substring(i))
+            path.pop()
+        }
+    }
+
+    backtrack([], s)
+    return res
+};
+
+let isPalindrome = (s) => {
+    let i = 0;
+    let j = s.length - 1
+
+    while(i < j){
+        if(s[i] != s[j]) return false
+        i++
+        j--
+    }
+
+    return true
+}
