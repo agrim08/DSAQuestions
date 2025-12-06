8b255d912626f7e850b74f47947a50cb81e8d14e
agrim08 <agrimgupta0805@gmail.com>
2025-12-07 03:51:52 +0530

    leetcode 57

---
+++ namaste_dsa/GreedyAlgorithm/insertIntervals.js
@@ -0,0 +1,24 @@
+var insert = function(arr, x) {
+    let n  = arr.length
+    let i = 0
+    let ans = []
+
+    while(i < n && arr[i][1] < x[0]){
+        ans.push(arr[i])
+        ++i
+    }
+
+    while(i < n && arr[i][0] <= x[1]){
+        x[0] = Math.min(arr[i][0], x[0])
+        x[1] = Math.max(arr[i][1], x[1])
+        ++i
+    }
+    ans.push(x)
+
+    while(i < n){
+        ans.push(arr[i])
+        ++i
+    }
+
+    return ans
+};
