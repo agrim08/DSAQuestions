0747cee1b029e2507506721759b8055c73e4276e
agrim08 <agrimgupta0805@gmail.com>
2025-12-07 23:56:48 +0530

    leetcode 134

---
+++ namaste_dsa/GreedyAlgorithm/gasStation.js
@@ -0,0 +1,18 @@
+var canCompleteCircuit = function(gas, cost) {
+    let currGain = 0
+    let totalGain = 0
+    ans = 0
+
+    for(let i = 0; i < gas.length; i++){
+        let gain = gas[i] - cost[i]
+        currGain = currGain + gain
+        totalGain = totalGain + gain
+
+        if(currGain < 0){
+            ans = i + 1
+            currGain = 0
+        }
+    }
+
+    return totalGain < 0 ? -1 : ans
+};
