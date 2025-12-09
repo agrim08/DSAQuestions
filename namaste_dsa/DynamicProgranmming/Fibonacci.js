f14f77126967df1b4067cb327e6c98e0c894a2bc
agrim08 <agrimgupta0805@gmail.com>
2025-12-10 01:40:21 +0530

    intro to dp

---
+++ namaste_dsa/DynamicProgranmming/Fibonacci.js
@@ -0,0 +1,20 @@
+//top-down
+let store = {}
+var fib = function(n) {
+    if(n <= 1) return n
+
+    if(!store[n]){
+        store[n] = fib(n - 1) + fib(n - 2)
+    }
+
+    return store[n]
+};
+
+//bottom-up
+var fib = function(n) {
+    let dp = [0, 1]
+    for(let i = 2; i <= n; i++){
+        dp[i] = dp[i - 1] + dp[i - 2]
+    }   
+    return dp[n]
+}
