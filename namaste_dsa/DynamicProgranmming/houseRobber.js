e2ad58e1893fffb496be68f7caadf410c702a5a1
agrim08 <agrimgupta0805@gmail.com>
2025-12-10 20:37:09 +0530

    optimization

--- namaste_dsa/DynamicProgranmming/houseRobber.js
+++ namaste_dsa/DynamicProgranmming/houseRobber.js
@@ -8,3 +8,17 @@ var rob = function(arr) {
 
     return dp[n - 1]
 };
+
+//O(1) space optimization
+var rob = function(arr) {
+    let n = arr.length
+    let prev1 = arr[0]
+    let prev2 = Math.max(arr[0], arr[1])
+
+    for(let i = 2; i < n; i++){
+        let curr = Math.max(arr[i] + prev1, prev2)
+        prev1 = prev2
+        prev2 = curr
+    }   
+    return prev2
+}
