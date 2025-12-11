b069a47bb9ae21cb87bf56a53ccaedfb1e962b1e
agrim08 <agrimgupta0805@gmail.com>
2025-12-11 18:07:41 +0530

    leetcode 322 recursive

---
+++ namaste_dsa/DynamicProgranmming/coinChange.js
@@ -0,0 +1,65 @@
+//brute force - TLE
+var coinChange = function(coins, amount) {
+    let n = coins.length
+    let calcMinCoin = (remAmount) => {
+        if(remAmount === 0) return 0
+        if(remAmount < 0) return -1
+
+        let minCoin = Infinity
+
+        for(let i = 0; i < n; i++){
+            let res = calcMinCoin(remAmount - coins[i])
+            if(res != -1){
+                minCoin = Math.min(minCoin, 1 + res)
+            }
+        }
+        return minCoin === Infinity ? -1 : minCoin
+    }
+    return calcMinCoin(amount)
+};
+
+//optimized with DP
+
+//*using Objects as store for memoization
+var coinChange = function(coins, amount) {
+    let n = coins.length
+    let store = {}
+
+    let calcMinCoin = (remAmount) => {
+        if(remAmount === 0) return 0
+        if(remAmount < 0) return -1
+
+        if(remAmount in store){
+            return store[remAmount]
+        }
+        let minCoin = Infinity
+        for(let i = 0; i < n; i++){
+            let res = calcMinCoin(remAmount - coins[i])
+            if(res != -1){
+                minCoin = Math.min(minCoin, 1 + res)
+            }
+        }
+        store[remAmount] = minCoin === Infinity ? -1 : minCoin
+        return store[remAmount]
+    }
+    return calcMinCoin(amount)
+};
+
+
+//*using Array as store for memoization (faster than object)
+var coinChange = function(coins, amount) {
+    let store = new Array(amount + 1).fill(undefined);
+    function calc(rem) {
+        if (rem === 0) return 0;
+        if (rem < 0) return -1;
+        if (store[rem] !== undefined) return store[rem];
+        let minCoin = Infinity;
+        for (let c of coins) {
+            let res = calc(rem - c);
+            if (res !== -1) minCoin = Math.min(minCoin, 1 + res);
+        }
+        store[rem] = minCoin === Infinity ? -1 : minCoin;
+        return store[rem];
+    }
+    return calc(amount);
+};
