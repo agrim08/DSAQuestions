38c08292349d3d104c755f9807a767c5b2f795da
agrim08 <agrimgupta0805@gmail.com>
2025-12-19 14:38:44 +0530

    leetcode 1971

---
+++ namaste_dsa/Graphs/FindifPathExists.js
@@ -0,0 +1,28 @@
+var validPath = function(n, edges, source, destination) {
+    let q = [source]
+    let visited = new Set()
+    let map = {}
+    //create map
+    for(let [x, y] of edges){
+        if(!map[x]) map[x] = []
+        if(!map[y]) map[y] = []
+
+        map[x].push(y)
+        map[y].push(x)
+    }
+
+    //start bfs
+    while(q.length){
+        let curr = q.shift()
+        if(curr === destination){
+            return true
+        }
+        for(let neighbor of map[curr]){
+            if(!visited.has(neighbor)){
+                q.push(neighbor)
+                visited.add(neighbor)
+            }
+        }
+    }
+    return false
+};
