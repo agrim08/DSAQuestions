d056d0e4c48a7dce786d4f9d6e577822d828df24
agrim08 <agrimgupta0805@gmail.com>
2025-12-20 23:04:21 +0530

    leetcode 64

---
+++ namaste_dsa/DynamicProgranmming/minimumPathSum.js
@@ -0,0 +1,21 @@
+var minPathSum = function(arr) {
+    if (!arr.length || !arr[0].length) return 0
+
+    let m = arr[0].length
+    let n = arr.length
+    let dp = Array.from({ length: n }, () => Array(m).fill(-1))
+
+    let fn = (x, y) => {
+        if(x === 0 && y === 0) return arr[0][0]
+        if(x < 0 || y < 0) return Infinity
+
+        if (dp[x][y] != -1) return dp[x][y]
+
+        let up = fn(x - 1, y) + arr[x][y]
+        let left = fn(x, y - 1) + arr[x][y]
+
+        return dp[x][y] = Math.min(up, left)
+    }
+
+    return fn(n - 1, m - 1)
+};
