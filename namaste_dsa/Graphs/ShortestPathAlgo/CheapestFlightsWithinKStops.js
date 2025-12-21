191c6b5068ab6f926cf30b19d494139eacad8324
agrim08 <agrimgupta0805@gmail.com>
2025-12-21 21:30:28 +0530

    approach 2: BFS

--- namaste_dsa/Graphs/ShortestPathAlgo/CheapestFlightsWithinKStops.js
+++ namaste_dsa/Graphs/ShortestPathAlgo/CheapestFlightsWithinKStops.js
@@ -1,3 +1,4 @@
+//approach 1: Bellman-Ford Algorithm
 var findCheapestPrice = function(n, flights, src, dst, k) {
     let dist = new Array(n).fill(Infinity)
     dist[src] = 0
@@ -14,3 +15,30 @@ var findCheapestPrice = function(n, flights, src, dst, k) {
 
     return dist[dst] === Infinity ? -1 : dist[dst]
 };
+
+//approach 2: BFS + store weight
+var findCheapestPrice = function(n, flights, src, dst, k) {
+    let graph = Array.from({length: n}, () => [])
+    for(let [from, to, cost] of flights){
+        graph[from].push([to, cost])
+    }
+
+    let q = [[src, 0, 0]] //node, cost, stops
+    let minCost = new Array(n).fill(Infinity)
+
+    while(q.length){
+        let [node, cost, stops] = q.shift()
+
+        //if stops exceed k, continue
+        if(stops > k) continue
+
+        for(let [neighbor, price] of graph[node]){
+            let newCost = cost + price
+            if(newCost < minCost[neighbor]){
+                minCost[neighbor] = newCost
+                q.push([neighbor, newCost, stops + 1])
+            }
+        }
+    }
+    return minCost[dst] === Infinity ? -1 : minCost[dst]
+}
