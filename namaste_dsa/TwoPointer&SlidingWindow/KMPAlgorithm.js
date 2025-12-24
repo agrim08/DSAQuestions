79a1969b2565fe363396a1b5e683831d5eba979c
agrim08 <agrimgupta0805@gmail.com>
2025-12-24 16:04:23 +0530

    KMP algo

---
+++ namaste_dsa/TwoPointer&SlidingWindow/KMPAlgorithm.js
@@ -0,0 +1,48 @@
+//* KMP Algorithm to find the first occurrence of needle in haystack
+var strStr = function(haystack, needle) {
+    let m = haystack.length
+    let n = needle.length
+    //* Edge case: if needle is an empty string
+    if(n == 0) return 0
+    let lps = []
+    lps[0] = 0
+
+    let i = 0
+    let j = 1
+    //*build lps array
+    while(j < n){
+        if(needle[i] == needle[j]){
+            lps[j] = i + 1
+            i++
+            j++
+        }else{
+            if(i == 0){
+                lps[j] = 0
+                ++j
+            }else{
+                //* backtrack i using lps array
+                i = lps[i - 1]
+            }
+        }
+    }
+    //* search for needle in haystack using lps array
+    i = 0
+    j = 0
+    while(i < m){
+        if(haystack[i] === needle[j]){
+            i++
+            j++
+        }else{
+            if(j == 0){
+                i++
+            }else{
+                j = lps[j - 1]
+            }
+        }
+        //* check if we have found the needle
+        if(j == n){
+            return i - n
+        }
+    }
+    return -1
+};
