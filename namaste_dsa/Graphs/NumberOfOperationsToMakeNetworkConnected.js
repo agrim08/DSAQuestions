121220f28fa4fd5f3fc65e604c73e46b5b4482f7
agrim08 <agrimgupta0805@gmail.com>
2025-12-27 23:42:07 +0530

    leetcode 1319

---
+++ namaste_dsa/Graphs/NumberOfOperationsToMakeNetworkConnected.js
@@ -0,0 +1,39 @@
+var makeConnected = function(n, connections) {
+    if(connections.length < n - 1){
+        return -1
+    }
+
+    let graph = Array.from({length: n}, () => [])
+
+    for(let [from, to] of connections){
+        graph[from].push(to)
+        graph[to].push(from)
+    }
+
+    let visited = new Array(n).fill(false)
+    let noOfComponents = 0
+
+    for(let i = 0; i < n; i++){
+        if(!visited[i]){
+            noOfComponents++
+            bfs(i, visited, graph)
+        }
+    }
+
+    return noOfComponents - 1
+};
+
+let bfs = (src, visited, graph) => {
+    let q = [src]
+    visited[src] = true
+    while(q.length){
+        let curr = q.shift()
+        visited[curr] = true
+        for(let neighbor of graph[curr]){
+            if(!visited[neighbor]){
+                visited[neighbor] = true
+                q.push(neighbor)
+            }
+        }
+    }
+}
