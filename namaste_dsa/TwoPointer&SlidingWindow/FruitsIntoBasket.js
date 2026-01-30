624bf79626142a24388027e9aa8ad489c9f4bd38
agrim08 <agrimgupta0805@gmail.com>
2026-01-30 20:56:32 +0530

    leetcode 904

---
+++ namaste_dsa/TwoPointer&SlidingWindow/FruitsIntoBasket.js
@@ -0,0 +1,23 @@
+var totalFruit = function(fruits) {
+    let left = 0
+    let max = 0
+    let map = new Map()
+
+    for(let right = 0; right < fruits.length; right++){
+        map.set(fruits[right], (map.get(fruits[right]) || 0) + 1)
+
+        while(map.size > 2){
+            map.set(fruits[left], (map.get(fruits[left]) || 0) - 1)
+
+            if(map.get(fruits[left]) === 0){
+                map.delete(fruits[left])
+            }
+
+            left++
+        }
+
+        max = Math.max(max, right - left + 1)
+    }
+
+    return max
+};
