df3267476d37f62530e274abd787506841ba8165
agrim08 <agrimgupta0805@gmail.com>
2026-02-15 00:03:00 +0530

    leetcode 973

---
+++ namaste_dsa/arrays/KNearestPointsToOrigin.js
@@ -0,0 +1,8 @@
+var kClosest = function(points, k) {
+    points.sort((a, b) => 
+        (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1])
+    );
+    
+    // Return first k points
+    return points.slice(0, k);
+};
