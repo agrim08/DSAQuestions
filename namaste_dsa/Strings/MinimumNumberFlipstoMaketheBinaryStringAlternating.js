29e2364c445bc57573f720d4c9fd0935df61f401
agrim08 <agrimgupta0805@gmail.com>
2026-03-07 17:20:22 +0530

    leetcode 1888 constant space

--- namaste_dsa/Strings/MinimumNumberFlipstoMaketheBinaryStringAlternating.js
+++ namaste_dsa/Strings/MinimumNumberFlipstoMaketheBinaryStringAlternating.js
@@ -1,10 +1,8 @@
 var minFlips = function (s) {
     let n = s.length
-    s = s + s
 
     let s1 = ""
     let s2 = ""
-
     for (let i = 0; i < n * 2; i++) {
         s1 += (i % 2 ? "0" : "1")
         s2 += (i % 2 ? "1" : "0")
@@ -13,22 +11,21 @@ var minFlips = function (s) {
     let res = Infinity
     let flip1 = 0
     let flip2 = 0
-
     let i = 0
     let j = 0
 
     while (j < 2 * n) {
-        if (s[j] !== s1[j]) {
+        if (s[j % n] !== s1[j]) { //trick to access circular elements
             flip1++
         }
-        if (s[j] !== s2[j]) {
+        if (s[j % n] !== s2[j]) {
             flip2++
         }
         if (j - i + 1 > n) {
-            if (s[i] !== s1[i]) {
+            if (s[i % n] !== s1[i]) {
                 flip1--
             }
-            if (s[i] !== s2[i]) {
+            if (s[i % n] !== s2[i]) {
                 flip2--
             }
             i++
@@ -38,5 +35,45 @@ var minFlips = function (s) {
         }
         j++
     }
+    return res
+};
+
+
+//O(1) space
+var minFlips = function (s) {
+    let n = s.length
+
+    let res = Infinity
+    let flip1 = 0
+    let flip2 = 0
+    let i = 0
+    let j = 0
+
+    while (j < 2 * n) {
+
+        let s1 = (j % 2 ? "0" : "1")
+        let s2 = (j % 2 ? "1" : "0")
+
+        if (s[j % n] !== s1) flip1++
+        if (s[j % n] !== s2) flip2++
+
+        if (j - i + 1 > n) {
+
+            let left1 = (i % 2 ? "0" : "1")
+            let left2 = (i % 2 ? "1" : "0")
+
+            if (s[i % n] !== left1) flip1--
+            if (s[i % n] !== left2) flip2--
+
+            i++
+        }
+
+        if (j - i + 1 === n) {
+            res = Math.min(res, flip1, flip2)
+        }
+
+        j++
+    }
+
     return res
 };
